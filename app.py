70cab6a0cad7cc2e249d7ec11dc97df588b4365e
David Dryburgh <david.dryburgh1994@gmail.com>
2019-08-28 21:02:29 +0100

    created put and patch methods

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ books = [
 ]
 
 # GET /books/{isbn}
-@app.route ('/books/<int:isbn>')
-def get_book_by_isbn(isbn, methods=['GET']):
+@app.route ('/books/<int:isbn>', methods=['GET'])
+def get_book_by_isbn(isbn):
     return_value = {}
     for book in books:
         if book["isbn"] == isbn:
@@ -37,7 +37,7 @@ def hello_world():
 @app.route('/books', methods=['POST'])
 def add_book():
     request_data = request.get_json()
-    if valid_book_object(request_data):
+    if _valid_book_object(request_data):
         new_book = {
             "name": request_data['name'],
             "price": request_data['price'],
@@ -55,8 +55,56 @@ def add_book():
         response = Response(json.dumps(invalid_book_object_error_msg), 400, mimetype='application/json')
         return response
 
+# PUT /books/[isbn]
+@app.route('/books/<int:isbn>', methods=['PUT'])
+def replace_book(isbn):
+    request_data = request.get_json()
+    if not _valid_put_request_data(request_data):
+        invalid_book_object_error_msg = {
+            "error": "Invalid book object passed in request.",
+            "helpString": "Data passed in similar to this {'name': 'book name', 'price': 7.99}"
+        }
+        response = Response(json.dumps(invalid_book_object_error_msg), 400, mimetype='application/json')
+        return response
+    new_book = {
+        'name': request_data['name'],
+        'price': request_data['price'],
+        'isbn': isbn
+    }
+    i = 0
+    for book in books:
+        current_isbn = book['isbn']
+        if current_isbn == isbn:
+            books[i] = new_book
+        i += 1
+    response = Response("", status=204)
+    return response
 
-def valid_book_object(book_object):
+# PATCH request for updating only fields that are passed
+@app.route('/books/<int:isbn>', methods=['PATCH'])
+def update_book(isbn):
+    request_data = request.get_json()
+    updated_book = {}
+    if "name" in request_data:
+        updated_book["name"] = request_data['name']
+    if "price" in request_data:
+        updated_book["price"] = request_data['price']
+    for book in books:
+        if book['isbn'] == isbn:
+            book.update(updated_book)
+    response = Response("", status=204)
+    response.headers['Location'] = "/books/" + str(isbn)
+    return response
+
+def _valid_put_request_data(book_object):
+    if ("name" in book_object and
+            "price" in book_object):
+        return True
+    else:
+        return False
+
+
+def _valid_book_object(book_object):
     if ("name" in book_object and
             "price" in book_object and
             "isbn" in book_object):
